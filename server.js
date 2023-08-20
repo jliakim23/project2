b487c7dd63b1c12410595ebba9e01eef0a6cb501
Lia Kim <j.liakim23@gmail.com>
2023-08-20 14:09:54 -0400

    deploying

--- server.js
+++ server.js
@@ -10,14 +10,21 @@ const methodOverride = require("method-override");
 const cors = require('cors');
 const bodyParser = require('body-parser');
 const multer = require("multer");
+const session = require('express-session')
+const MemoryStore = require('memorystore')(session)
+
 app.set("view engine", "ejs");
 
 // middlewares
 app.use(express.static("public"));
 app.use(expressLayouts);
 app.use(
-  // one hour login time
-  session({ secret: "somestringreandomdwd", cookie: { maxAge: 3600000 } })
+  session({
+    secret: "somestringreandomdwd",
+    resave: false, 
+    saveUninitialized: true, 
+    cookie: { maxAge: 3600000 },
+  })
 );
 app.use(methodOverride("_method"));
 
@@ -25,7 +32,14 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(cors());
-
+app.use(session({
+  cookie: { maxAge: 86400000 },
+  store: new MemoryStore({
+    checkPeriod: 86400000 
+  }),
+  resave: false,
+  secret: 'keyboard cat'
+}))
 app.use(authRoutes);
 
 app.get("/", (req, res) => {
